bf54f6b83346d6b8057af237bd44fc9be7891181
Schmxdt <airtonschmidt2018@gmail.com>
2025-04-22 14:02:44 -0300

    feat: Route to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout'; 
+ 
+import DashboardPage from './pages/dashboard/pages/DashboardPage';
+import ChamadosPage from './pages/chamados/pages/ChamadosPage';
+import SituacoesPage from './pages/situacoes/pages/SituacoesPage';
+import CategoriasPage from './pages/categorias/pages/CategoriasPage';
+import ChamadosFormPage from './pages/chamados/pages/ChamadosFormPage';
+import SituacoesFormPage from './pages/situacoes/pages/SituacoesFormPage';
+import CategoriasFormPage from './pages/categorias/pages/CategoriasFormPage';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {/* // Dashboard */}
+          <Route path="dashboard" element={<DashboardPage />} />
+
+          {/* // Chamados */}
+          <Route path="chamados" element={<ChamadosPage />} />
+          <Route path="chamados/:id" element={<ChamadosFormPage />} />
+          <Route path="chamados/new" element={<ChamadosFormPage />} />
+
+          {/* // Situações */}
+          <Route path="situacoes" element={<SituacoesPage />} />
+          <Route path="situacoes/:id" element={<SituacoesFormPage />} />
+          <Route path="situacoes/new" element={<SituacoesFormPage />} />
+
+          {/* // Categorias */}
+          <Route path="categorias" element={<CategoriasPage />} />
+          <Route path="categorias/:id" element={<CategoriasFormPage />} />
+          <Route path="categorias/new" element={<CategoriasFormPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
