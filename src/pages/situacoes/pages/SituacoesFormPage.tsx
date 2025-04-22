fc0793f8cadade38b03440238f4042b6bb21dd17
Schmxdt <airtonschmidt2018@gmail.com>
2025-04-22 14:03:36 -0300

    feat: Situacoes Page

---
+++ src/pages/situacoes/pages/SituacoesFormPage.tsx
@@ -0,0 +1,68 @@
+import { useState, FormEvent, useEffect } from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Situacao } from '../../../models/situacao';
+import { api } from '../../../services/api';
+
+export default function SituacoesFormPage() {
+  const { id } = useParams();
+  const [nome, setNome] = useState('');
+  const tableName = "situacoes";
+  const navigate = useNavigate();
+
+  const handleSubmit = async (data: Situacao) => {
+    try {
+      if (id) {
+        await api.put(`/${tableName}/${id}`, data);
+      }
+      else {
+        await api.post(`/${tableName}`, data);
+      }
+      navigate(`/${tableName}`);
+    }
+    catch (error) {
+      console.error("Error submitting answers:", error);
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        try {
+          const response = await api.get(`/${tableName}/${id}`);
+          setNome(response.data.nome);
+        }
+        catch (error) {
+          console.error("Error fetching situacao:", error);
+        }
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  return (
+    <Box component="form" sx={{ maxWidth: 600 }}>
+      <Typography variant="h5" gutterBottom>
+        {id ? 'Editar Situação' : 'Novo Situação'}
+      </Typography>
+
+      <TextField
+        label="Nome"
+        fullWidth
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        margin="normal"
+        required
+      />
+
+      <Box mt={2}>
+        <Button type="submit" onClick={(e) => { e.preventDefault(); handleSubmit({ nome }); }} variant="contained" color="primary">
+          {id ? 'Salvar Alterações' : 'Criar Situação'}
+        </Button>
+        <Button sx={{ ml: 2 }} onClick={() => navigate(`/${tableName}`)}>
+          Cancelar
+        </Button>
+      </Box>
+    </Box>
+  );
+}
